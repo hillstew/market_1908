e629550348d71d2a5a04cd8e3ee4bf95878bb3b4
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-11 13:52:20 -0600

    Add tests for Market

---
+++ test/market_test.rb
@@ -0,0 +1,51 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+require './lib/market'
+
+class MarketTest < Minitest::Test
+  def setup
+    @market = Market.new("South Pearl Street Farmers Market")
+    @vendor_1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor_2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor_3 = Vendor.new("Palisade Peach Shack")
+    @vendor_1.stock("Peaches", 35)
+    @vendor_1.stock("Tomatoes", 7)
+    @vendor_2.stock("Banana Nice Cream", 50)
+    @vendor_2.stock("Peach-Raspberry Nice Cream", 25)
+    @vendor_3.stock("Peaches", 65)
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+
+  def test_it_has_a_name
+    assert_equal "South Pearl Street Farmers Market", @market.name
+  end
+
+  def test_it_has_no_vendors_to_start
+    assert_equal [], @market.vendors
+  end
+
+  def test_it_can_add_vendors
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    assert_equal [@vendor_1, @vendor_2, @vendor_3], @market.vendors
+  end
+
+  def test_it_can_return_the_names_of_vendors
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    assert_equal ["Rocky Mountain Fresh", "Ba-Nom-a-Nom", "Palisade Peach Shack"], @market.vendor_names
+  end
+
+  def test_it_can_return_vendors_with_specific_produce
+    @market.add_vendor(@vendor_1)
+    @market.add_vendor(@vendor_2)
+    @market.add_vendor(@vendor_3)
+    assert_equal [@vendor_1, @vendor_3], @market.vendors_that_sell("Peaches")
+  end
+end
