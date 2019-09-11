5e7120d75d63799db95f5ec0a6263e5d5e7ea88e
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-11 14:43:06 -0600

    Add sorted_item_list, total_inventory, and sell methods to Market

--- lib/market.rb
+++ lib/market.rb
@@ -20,5 +20,36 @@ class Market
     end
   end
 
-  
+  def sorted_item_list
+    @vendors.map { |vendor| vendor.inventory.keys }.flatten.uniq.sort { |a, b| a <=> b }
+  end
+
+  def total_inventory
+    total_items = Hash.new
+    total = @vendors.each do |vendor|
+      vendor.inventory.each do |type, amount|
+        if total_items.has_key?(type)
+          total_items[type] += amount
+        elsif !total_items.has_key?(type)
+          total_items[type] = amount
+        end
+      end
+    end
+    total_items
+  end
+
+  def sell(produce_type, amount)
+    return false if !total_inventory.has_key?(produce_type) || total_inventory[produce_type] < amount
+
+    @vendors.each do |vendor|
+      if vendor.inventory.has_key?(produce_type)
+        if vendor.inventory[produce_type] >= amount
+          vendor.inventory[produce_type] -= amount
+        elsif !(vendor.inventory[produce_type] >= amount)
+          amount -= vendor.inventory[produce_type]
+          vendor.inventory[produce_type] -= vendor.inventory[produce_type]
+        end
+      end
+    end
+  end
 end
