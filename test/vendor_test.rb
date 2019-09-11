f36c08476925783fe8c22dd6bb543a0d208e7c68
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-11 13:14:30 -0600

    Add test for no inventory and name

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -10,4 +10,14 @@ class VendorTest < Minitest::Test
   def test_it_exists
     assert_instance_of Vendor, @vendor
   end
+
+  def test_it_has_a_name
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+  end
+
+  def test_it_has_no_inventory
+    empty_hash = {}
+    assert_equal empty_hash, @vendor.inventory
+  end
+
 end
